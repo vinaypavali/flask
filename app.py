085351bd27a7eda3791077a2f8da0ebd68a91fb2
Vinay Kumar <pavalivinay6@gmail.com>
2023-09-07 01:49:09 +0530

    init

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask 
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def hello():
+    return "Binance API"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
